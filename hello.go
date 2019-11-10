780a70ec5231e56e70dff671e672148983700f01
Saleem Siddiqui <Siddiqui.Saleem@gmail.com>
2019-11-10 13:14:16 -0600

    Hello world with basic operations on strings and numeric types

--- hello.go
+++ hello.go
@@ -1,7 +1,17 @@
 	package main
 
-	import "fmt"
+import "fmt"
 
-	func main() {
-		fmt.Println("Hello World!")
-	}
+func main() {
+	fmt.Println("Hello, " + "World!")
+
+	fmt.Println("1+10 =", 1+10)
+	fmt.Println("7/3.0 =", 7/3.0)
+	fmt.Println("7/3 =", 7/3)
+	fmt.Println("7%3 =", 7%3)
+	fmt.Println("7^3 =", 7^3)
+	fmt.Println("7*3 =", 7*3)
+	fmt.Println("true AND false is", true && false)
+	fmt.Println("true OR false is", true || false)
+	fmt.Println("not true is", !true)
+}
