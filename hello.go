4b17c8e459db15d101c2872709bd2308b6f09699
Saleem Siddiqui <Siddiqui.Saleem@gmail.com>
2019-11-10 13:23:11 -0600

    Hello world with variables and constants

--- hello.go
+++ hello.go
@@ -1,17 +1,31 @@
 	package main
 
-import "fmt"
+import (
+  f "fmt"
+  "math"
+)
 
 func main() {
-	fmt.Println("Hello, " + "World!")
+	const h = "Hello, " + "World!"
+	f.Println(h)
 
-	fmt.Println("1+10 =", 1+10)
-	fmt.Println("7/3.0 =", 7/3.0)
-	fmt.Println("7/3 =", 7/3)
-	fmt.Println("7%3 =", 7%3)
-	fmt.Println("7^3 =", 7^3)
-	fmt.Println("7*3 =", 7*3)
-	fmt.Println("true AND false is", true && false)
-	fmt.Println("true OR false is", true || false)
-	fmt.Println("not true is", !true)
+	const y, z = 7/3.0, 7.0/4
+    f.Println("7/3.0 =", y)
+    f.Println("7.0/4 =", z)
+
+    const i, j, k, l, m = 1+10, 7/3, 7%3, 7^3, 7*3
+	f.Println("1+10 =", i)
+	f.Println("7/3 =", j)
+	f.Println("7%3 =", k)
+	f.Println("7^3 =", l)
+	f.Println("7*3 =", m)
+
+    const b, c, d =  true && false, true || false, !true
+	f.Println("true AND false is", b)
+	f.Println("true OR false is", c)
+	f.Println("not true is", d)
+
+	// Cannot be defined as a constant
+	xkcd217 := math.Exp(math.Pi) - math.Pi
+	f.Println("www.xkcd.com/217 punchline:", xkcd217)
 }
