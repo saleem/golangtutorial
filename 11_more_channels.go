b7d3d9affa9b730ff2b24ca0ce357e257f74b462
Saleem Siddiqui <Siddiqui.Saleem@gmail.com>
2020-01-02 17:40:51 -0600

    Timeouts and non-blocking channels

--- 11_more_channels.go
+++ 11_more_channels.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	f "fmt"
-	// "math/rand"
-	// "time"
+	"time"
 )
 
 func MoreChannels() {
 	channelSelector()
+	timeouts()
+	nonBlocking()
 }
 
 func channelSelector() {
+	f.Println("********** Channel Selectors **********")
+
 	c1 := make(chan string)
 	c2 := make(chan string)
 
@@ -24,5 +27,60 @@ func channelSelector() {
 		case msg2 := <-c2:
 			f.Println("received", msg2)
 		}
+	}
+}
+
+func timeouts() {
+	f.Println("********** Timeouts **********")
+	willTimeOut()
+	willNotTimeOut()
+}
+
+func willTimeOut() {
+	createAndWait("will timeout", 2, 1)
+}
+
+func willNotTimeOut() {
+	createAndWait("will not timeout", 2, 3)
+}
+
+func createAndWait(msg string, creationWait time.Duration, timeoutWait time.Duration) {
+	c2 := make(chan string, 1)
+	go producer(c2, msg, creationWait)
+	select {
+	case res := <-c2:
+		f.Println("received message: " + res)
+	case <-time.After(timeoutWait * time.Second):
+		f.Println("timed out for: " + msg)
+	}
+}
+
+func nonBlocking() {
+	f.Println("********** Non-blocking Channels **********")
+
+	messages := make(chan string) // no buffer
+	signals := make(chan string)  // no buffer
+	select {
+	case msg := <-messages:
+		f.Println("received message", msg)
+	default:
+		f.Println("no message received")
+	}
+
+	msg := "aloha"
+	select {
+	case messages <- msg:
+		f.Println("sent message", msg)
+	default:
+		f.Println("no message sent")
+	}
+
+	select {
+	case msg := <-messages:
+		f.Println("received message", msg)
+	case sig := <-signals:
+		f.Println("received signal", sig)
+	default:
+		f.Println("no activity")
 	}
 }
